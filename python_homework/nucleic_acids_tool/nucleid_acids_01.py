4b906d6b232436822c862538ebbd17c9fbbecdbb
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-09-17 22:18:24 +0300

    added classes for DNA and RNA comands

--- python_homework/nucleic_acids_tool/nucleid_acids_01.py
+++ python_homework/nucleic_acids_tool/nucleid_acids_01.py
@@ -1,10 +1,66 @@
 #simple work with RNA
 
-# Reads commands from the user in an endless loop.
-# After the command, the program prompts the user for the nucleic acid sequence, converts it, and outputs the result.
-# Program preserves case (e.g. complement AtGc is TaCg)
-# Works only with DNA OR RNA, not with mixed.
+class DNAOperations:
 
+    def reverse(self, sequence):
+        'Makes reverse sequence.'
+        return sequence[::-1]
+
+
+    def complement(self, sequence):
+        'Makes complement sequence. If u or U in sequence - works like with RNA, otherwise, like with DNA.'
+        dna_dict = {'a':'t', 't':'a', 'c':'g', 'g':'c', 'A':'T', 'T':'A', 'C':'G', 'G':'C'}
+        answer = ''
+    
+        for letter in sequence:
+                answer += dna_dict[letter]
+        
+        return answer
+
+
+    def transcribe(self, sequence):
+        'Transcribes DNA sequence to RNA sequence.'
+        dna_dict = {'a':'u', 't':'a', 'c':'g', 'g':'c', 'A':'U', 'T':'A', 'C':'G', 'G':'C'}
+        answer = ''
+
+        for letter in sequence:
+            answer += dna_dict[letter]
+    
+        return answer
+
+
+    def reverse_complement(self, sequence):
+        'Makes reverse complement sequence.'
+        return self.complement(self.reverse(sequence))
+
+
+class RNAOperations:
+
+    def reverse(self, sequence):
+        'Makes reverse sequence.'
+        return sequence[::-1]
+
+
+    def complement(self, sequence):
+        'Makes complement sequence. If u or U in sequence - works like with RNA, otherwise, like with DNA.'
+        rna_dict = {'a':'u', 'u':'a', 'c':'g', 'g':'c', 'A':'U', 'U':'A', 'C':'G', 'G':'C'}
+        answer = ''
+        
+        for letter in sequence:
+            answer += rna_dict[letter]
+        
+        return answer
+
+
+    def transcribe(self, sequence):
+        'Can not ranscribe RNA sequence. Print "Try again".'
+        return 'Oops, I don`t know how to transcribe RNA. Try again.'
+
+
+    def reverse_complement(self, sequence):
+        'Makes reverse complement sequence.'
+        return self.complement(self.reverse(sequence))
+    
 
 def check_na(sequence):
     'checks that nucleic acid is DNA OR RNA. If so, returns 1 otherwise, returns 0'
@@ -14,52 +70,15 @@ def check_na(sequence):
     short_seq = set(sequence.upper()) - na_letters
 
     if len(short_seq) > 1:
-        return 0
+        return False
     
     if len(short_seq) == 1 and not short_seq <= dr_letter:
-        return 0
+        return False
 
-    return 1
+    if short_seq == {'U'}:
+        return RNAOperations()
 
-
-def reverse(sequence):
-    'makes reverse sequence'
-    return sequence[::-1]
-
-
-def complement(sequence):
-    'makes complement sequence. If u or U in sequence - works like with RNA, otherwise, like with DNA'
-    dna_dict = {'a':'t', 't':'a', 'c':'g', 'g':'c', 'A':'T', 'T':'A', 'C':'G', 'G':'C'}
-    rna_dict = {'a':'u', 'u':'a', 'c':'g', 'g':'c', 'A':'U', 'U':'A', 'C':'G', 'G':'C'}
-
-    answer = ''
-
-    if 'u' in sequence or 'U' in sequence:
-        for letter in sequence:
-            answer += rna_dict[letter]
-    else:
-        for letter in sequence:
-            answer += dna_dict[letter]
-
-    return answer
-
-
-def reverse_complement(sequence):
-    'makes reverse complement sequence'
-    return complement(reverse(sequence))
-
-
-def transcribe(sequence):
-    'transcribes DNA sequence'
-    dna_dict = {'a':'u', 't':'a', 'c':'g', 'g':'c', 'A':'U', 'T':'A', 'C':'G', 'G':'C'}
-    answer = ''
-
-    if 'u' in sequence or 'U' in sequence:
-        return 'Oops, I don`t know how to transcribe RNA. Try again.'
-    else:
-        for letter in sequence:
-            answer += dna_dict[letter]
-        return answer
+    return DNAOperations()
 
 
 def main():
@@ -67,42 +86,40 @@ def main():
     print('Now I show your available commands:')
     print('you can "reverse" - "r" or "transcribe" - "t" your sequence and make "complement" - "c" or "reverse_complement" - "rc"')
     
-    'reads the first command to work on DNA or RNA'
-    command = str(input('Chose command or "exit": '))
+    #reads the first command to work on DNA or RNA
+    command = str(input('Choose command or "exit": '))
 
     while command != 'exit':
         
-        'reads the sequence and checks whether it is RNA or DNA'
+        #reads the sequence and checks whether it is RNA or DNA
         sequence = str(input('Now, please enter a sequence: '))
-        correct_check = check_na(sequence)
+        operations = check_na(sequence)
         
-        if correct_check:
-            print('Great, lets work with nucleic acid')
-        else:
+        if not operations:
             if sequence == 'exit':
                 command = sequence
             else:
                 print('Oops, It`s not a DNA or RNA. Try again.')
             continue
-        
-        'executes the required command on the given sequence'
+
         if command == 'reverse' or command == 'r':
-            print(reverse(sequence))
-            
+            print(operations.reverse(sequence))
+
         elif command == 'complement' or command == 'c':
-            print(complement(sequence))
-        
+            print(operations.complement(sequence))
+    
         elif command == 'reverse_complement' or command == 'rc':
-            print(reverse_complement(sequence))
-        
+            print(operations.reverse_complement(sequence))
+    
         elif command == 'transcribe' or command == 't':
-            print(transcribe(sequence))
+            print(operations.transcribe(sequence))
 
         else:
-            print('Oops, something wrong. Please, chose another command')
+            print('Oops, something wrong. Please, choose another command')
 
-        'Asks next command and moves to the next cycle'
-        command = str(input('Chose command or "exit": '))
+
+        #Asks next command and moves to the next cycle
+        command = str(input('Choose command or "exit": '))
 
 
     print('Goodbye!')
--- python_homework/nucleic_acids_tool/test_01.py
+++ python_homework/nucleic_acids_tool/test_01.py
@@ -1,46 +1,60 @@
-#pytest python_homework/nucleid_acids_tool
+#pytest python_homework/nucleic_acids_tool
 
 
 import pytest
-from .nucleid_acids_01 import check_na, reverse, complement, reverse_complement, transcribe
+from .nucleid_acids_01 import *
 
 
 def test_check_na():
-    assert check_na('tTgggcCAAAAAaa') == 1
-    assert check_na('AUGC') == 1
-    assert check_na('aaaaaa') == 1
-    assert check_na('GccccUA') == 1
-    assert check_na('AuT') == 0
-    assert check_na('UUUUUU') == 1
-    assert check_na('Ac9G') == 0
-    assert check_na('9hg3') == 0
+    assert isinstance(check_na('tTgggcCAAAAAaa'), DNAOperations) == 1
+    assert isinstance(check_na('AUGC'), RNAOperations) == 1
+    assert isinstance(check_na('aaaaaa'), DNAOperations) == 1
+    assert isinstance(check_na('GccccUA'), RNAOperations) == 1
+    assert isinstance(check_na('AuT'), DNAOperations) == 0
+    assert isinstance(check_na('UUUUUU'), RNAOperations) == 1
+    assert isinstance(check_na('9hg3'), DNAOperations) == 0
 
 
-def test_reverse():
-    assert reverse('tTgggcCAAAAAaa') == 'aaAAAAACcgggTt'
-    assert reverse('AUGC') == 'CGUA'
-    assert reverse('aaaaaa') == 'aaaaaa'
-    assert reverse('GccccUA') == 'AUccccG'
+def test_DNAreverse():
+    assert DNAOperations().reverse('tTgggcCAAAAAaa') == 'aaAAAAACcgggTt'
+    assert DNAOperations().reverse('aaaaaa') == 'aaaaaa'
+    assert DNAOperations().reverse('c') == 'c'
 
 
-def test_complement():
-    assert complement('tTgggcCAAAAAaa') == 'aAcccgGTTTTTtt'
-    assert complement('AUGC') == 'UACG'
-    assert complement('aaaaaa') == 'tttttt'
-    assert complement('GccccUA') == 'CggggAU'
+def test_RNAreverse():
+    assert RNAOperations().reverse('AUGC') == 'CGUA'
+    assert RNAOperations().reverse('ugC') == 'Cgu'
+    assert RNAOperations().reverse('U') == 'U'
 
 
-def test_reverse_complement():
-    assert reverse_complement('tTgggcCAAAAAaa') == 'ttTTTTTGgcccAa'
-    assert reverse_complement('U') == 'A'
-    assert reverse_complement('AUGC') == 'GCAU'
-    assert reverse_complement('aaaaaa') == 'tttttt'
-    assert reverse_complement('GccccUA') == 'UAggggC'
+def test_DNAcomplement():
+    assert DNAOperations().complement('tTgggcCAAAAAaa') == 'aAcccgGTTTTTtt'
+    assert DNAOperations().complement('aaaaaa') == 'tttttt'
 
 
-def test_transcribe():
-    assert transcribe('tTgggcCAAAAAaa') == 'aAcccgGUUUUUuu'
-    assert transcribe('AUGC') == 'Oops, I don`t know how to transcribe RNA'
-    assert transcribe('aaaaaa') == 'uuuuuu'
-    assert transcribe('A') == 'U'
+def test_RNAcomplement():
+    assert RNAOperations().complement('AUGC') == 'UACG'
+    assert RNAOperations().complement('GccccUA') == 'CggggAU'
+
+
+def test_DNAreverse_complement():
+    assert DNAOperations().reverse_complement('tTgggcCAAAAAaa') == 'ttTTTTTGgcccAa'
+    assert DNAOperations().reverse_complement('t') == 'a'
+    assert DNAOperations().reverse_complement('Aaaaaa') == 'tttttT'
+
+
+def test_RNAreverse_complement():
+    assert RNAOperations().reverse_complement('U') == 'A'
+    assert RNAOperations().reverse_complement('AUGC') == 'GCAU'
+    assert RNAOperations().reverse_complement('GccccUA') == 'UAggggC'
+
+
+def test_DNAtranscribe():
+    assert DNAOperations().transcribe('tTgggcCAAAAAaa') == 'aAcccgGUUUUUuu'
+    assert DNAOperations().transcribe('aaaaaa') == 'uuuuuu'
+    assert DNAOperations().transcribe('A') == 'U'
     
+
+def test_RNAtranscribe():
+    assert RNAOperations().transcribe('AUGC') == 'Oops, I don`t know how to transcribe RNA. Try again.'
+    assert RNAOperations().transcribe('U') == 'Oops, I don`t know how to transcribe RNA. Try again.'
