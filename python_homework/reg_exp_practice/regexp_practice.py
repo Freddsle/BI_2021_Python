66f33e0abe4fea310c749ab4ad199b4df876cfe0
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-11-23 19:56:34 +0300

    all tasks added (plot)

--- python_homework/reg_exp_practice/regexp_practice.py
+++ python_homework/reg_exp_practice/regexp_practice.py
@@ -1,5 +1,8 @@
 import urllib.request
 import re
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def file_dowload(url, file_reference):
@@ -11,34 +14,41 @@ def file_dowload(url, file_reference):
         out_file.write(data)
 
 
-def read_write(file_input, file_output, match_regexp):
+def write_matches(file_output, matches):
+    with open(file_output, 'a') as out_file:
+        out_file.writelines('\n'.join(map(str, matches)))
+
+
+def read_file_lines(file_input, match_regexp):
+    '''
+    It parse input file and write to output file all regexp matches.
+    '''
     all_matches = []
 
     with open(file_input, 'r') as ref_file:
         for line in ref_file:
             find_list = re.findall(match_regexp, line)
             if find_list:
-                all_matches.append('\n'.join(map(str, find_list)))
+                all_matches.extend(find_list)
 
-    with open(file_output, 'a') as out_file:
-        out_file.writelines('\n'.join(map(str, all_matches)))
+    return all_matches
 
 
 def ftp_parser(url):
     '''
-    Parse the references file and write all "ftp" links from there to the "ftps" file.
+    Parses the references file and write all "ftp" links from there to the "ftps" file.
     '''
     file_reference = 'python_homework/reg_exp_practice/files/references.txt'
     file_dowload(url, file_reference)
+
     file_output = 'python_homework/reg_exp_practice/files/ftps.txt'
+    ftp_matches = read_file_lines(file_reference, re.compile(r'\bftp[#\.\w\/]+\b'))
+    write_matches(file_output, ftp_matches)
 
-    ftp_regexp = re.compile(r'\bftp[#\.\w\/]+\b')
-    read_write(file_reference, file_output, ftp_regexp)
 
-
-def numbers_parser(url):
+def tale_parser(url):
     '''
-    Parse the tale file and write all numbers from there to the .txt file.
+    Parses the tale file and write all numbers from there to the .txt file.
     '''
     file_tale = 'python_homework/reg_exp_practice/files/tale.txt'
     file_dowload(url, file_tale)
@@ -46,12 +56,42 @@ def numbers_parser(url):
     # Extract from the tale 2430 A.D. all numbers
     file_output_numbers = 'python_homework/reg_exp_practice/files/tale_numbers.txt'
     number_regexp = re.compile(r'(?<=\D)\d+\.{0,1}\d*(?=\D)')
-    read_write(file_tale, file_output_numbers, number_regexp)
+    number_matches = read_file_lines(file_tale, number_regexp)
+    write_matches(file_output_numbers, number_matches)
 
     # From the same tale, extract all words that contain the letter a, case is not important
     file_output_words = 'python_homework/reg_exp_practice/files/tale_a_words.txt'
     a_word_regexp = re.compile(r'(?<=\b)\w*[aA][\w\']*(?=\b)')
-    read_write(file_tale, file_output_words, a_word_regexp)
+    a_word__matches = read_file_lines(file_tale, a_word_regexp)
+    write_matches(file_output_words, a_word__matches)
+
+    # Extract all exclamation sentences from the story
+    file_output_ex = 'python_homework/reg_exp_practice/files/tale_exclamation.txt'
+    ex_regexp = re.compile(r'(?<=[(\s\"])[A-Z]+[\w\s\'\,]*!(?=[\s\"])')
+    ex_matches = read_file_lines(file_tale, ex_regexp)
+    write_matches(file_output_ex, ex_matches)
+
+    # Plot a histogram of the distribution of the lengths of unique words (case-insensitive, length from 1) in the text.
+    words_regexp =  re.compile(r'\b[A-Za-z_\']+\b|(?<=\D)\d+\.{0,1}\d*(?=\D)')
+    words = read_file_lines(file_tale, words_regexp)
+    words = set([word.lower() for word in read_file_lines(file_tale, words_regexp)])
+    uniqe_words_plot(words)
+
+
+def uniqe_words_plot(uniqe_wors):
+    '''
+    Plots a histogram of the distribution of the lengths of unique words (case-insensitive, length from 1) in the text.
+    '''
+    data = pd.DataFrame(uniqe_wors, columns=['word'])
+    data['word_len'] = data['word'].str.len()
+
+    plt.margins(0)
+    sns.displot(data['word_len'], stat='frequency')
+    plt.title('Distribution of lengths of unique words')
+    plt.xlabel('word lenght')
+    plt.gcf().set_size_inches(8, 6)
+    plt.savefig('python_homework/reg_exp_practice/files/Words lenght density.png', dpi=100, bbox_inches='tight')
+    plt.close()
 
 
 def main():
@@ -63,7 +103,7 @@ def main():
     
     # https://raw.githubusercontent.com/Serfentum/bf_course/master/15.re/2430AD
     url = 'https://raw.githubusercontent.com/Serfentum/bf_course/master/15.re/2430AD'
-    numbers_parser(url)
+    tale_parser(url)
 
 
 if __name__ == '__main__':
