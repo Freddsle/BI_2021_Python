b6ff3cf8a87e4b78e6a34537cd5679827f4ef66e
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-12-05 17:28:05 +0300

    added func_chain, conditional_reduce and consensus_filter

--- python_homework/functional_programming/functional.py
+++ python_homework/functional_programming/functional.py
@@ -1,57 +1,78 @@
 
-def sequential_map(*functions):
+def func_chain(*args):
     '''
-    The function accepts as arguments any number of functions (positional arguments, NOT a list), 
+    The function accepts any number of functions as arguments (positional arguments, NOT a list).
+    The function returns a function that concatenates all passed in sequential execution.
+    For example, my_chain = func_chain(lambda x: x + 2, lambda x: (x/4, x//4)).
+    my_chain(37) -> (9.75, 9).
+    '''
+    def inside_chain_func(inside_container):
+        if not inside_container:
+            return
+
+        if isinstance(inside_container, (int, float)):
+            for function in args:
+                inside_container = function(inside_container)
+
+        else:
+            for function in args:
+                inside_container = list(map(function, inside_container))
+
+        return inside_container
+
+    return inside_chain_func
+
+
+def sequential_map(*args):
+    '''
+    The function accepts as arguments any number of functions (positional arguments, NOT a list),
     as well as a container with some values.
-    The function returns a list of the results of sequential application of the passed functions to the values in the container. 
+    The function returns a list of the results of sequential application of the passed functions to the values in the container.
     For example: sequential_map(np.square, np.sqrt, lambda x: x**3, [1, 2, 3, 4, 5]) -> [1, 8, 27, 64, 125]
-    !!! Дополнительно - интегрировать сюда func_chain.
     '''
-    new_container_values = functions[-1].copy()
-
-    for function in functions[:-1]:
-        new_container_values = list(map(function, new_container_values))
-
-    return new_container_values
+    new_container_values = args[-1]
+    now_func_chain = func_chain(*args[:-1])
+    return now_func_chain(new_container_values)
 
 
-def consensus_filter(*functions):
+def consensus_filter(*args):
     '''
-    The function accepts as arguments any number of functions (positional arguments, NOT a list) 
+    The function accepts as arguments any number of functions (positional arguments, NOT a list)
     that return True or False, as well as a container with some values.
     The function returns a list of values that, when passed to all functions, give True.
     For example: consensus_filter(lambda x: x > 0, lambda x: x > 5, lambda x: x < 10, [-2, 0, 4, 6, 11]) -> [6]
     '''
-    new_container_values = functions[-1].copy()
+    new_container_values = args[-1]
 
-    for function in functions[:-1]:
+    if not new_container_values:
+        return
+
+    for function in args[:-1]:
         new_container_values = list(filter(function, new_container_values))
 
     return new_container_values
 
 
-def conditional_reduce():
+def conditional_reduce(func_one, func_two, values):
     '''
-    функция должна принимать 2 функции, а также контейнер с значениями. 
-    Первая функция должна принимать 1 аргумент и возвращать True или False, 
-    вторая также принимает 2 аргумента и возвращает значение (как в обычной функции reduce). 
-    conditional_reduce должна возвращать одно значение - результат reduce, 
-    пропуская значения с которыми первая функция выдала False. 
-    Например, conditional_reduce(lambda x: x < 5, lambda x, y: x + y, [1, 3, 5, 10]) -> 4
+    The function takes 2 functions and also a container with values.
+    The first function must take 1 argument and return True or False,
+    the second takes 2 arguments and returns a value (just like in a normal reduce function).
+    conditional_reduce returns one value - the result of reduce, skipping the values with which the first function returned False.
+    For example, conditional_reduce(lambda x: x < 5, lambda x, y: x + y, [1, 3, 5, 10]) -> 4
     '''
-    pass
+    new_container_values = list(filter(func_one, values))
 
+    if not values:
+        return
 
-def func_chain(*args):
-    '''
-    функция должна принимать в качестве аргументов любое количество функций (позиционными аргументами, НЕ списком). 
-    Функция должна возвращать функцию, объединяющую все переданные последовательным выполнением. 
-    Например, 
-    my_chain = func_chain(lambda x: x + 2, lambda x: x/4, x//4). 
-    my_chain(37) -> (9.75, 9). 
-    !!! +2 дополнительных балла за интеграцию этой функции в 1 задание. !!!
-    '''
-    pass
+    for i in range(len(new_container_values)):
+        if i == 0:
+            new_number = new_container_values[i]
+        else:
+            new_number = func_two(new_number, new_container_values[i])
+
+    return new_number
 
 
 def multiple_partial(*args):
--- python_homework/functional_programming/test_funcpractice.py
+++ python_homework/functional_programming/test_funcpractice.py
@@ -7,24 +7,29 @@ from functional_programming import functional
 def test_sequential_map():
     assert functional.sequential_map(np.square, np.sqrt, lambda x: x**3, [1, 2, 3, 4, 5]) == [1, 8, 27, 64, 125]
     assert functional.sequential_map(lambda x: x**2, [1, 2]) == [1, 4]
-    assert functional.sequential_map(lambda x: x**2, []) == []
+    assert functional.sequential_map(lambda x: x**2, []) is None
 
 
 def test_consensus_filter():
     assert functional.consensus_filter(lambda x: x > 0, lambda x: x > 5, lambda x: x < 10, [-2, 0, 4, 6, 11]) == [6]
     assert functional.consensus_filter(lambda x: x > 0, [-2, 0, 4, 6, 11]) == [4, 6, 11]
-    assert functional.consensus_filter(lambda x: x > 0, []) == []
+    assert functional.consensus_filter(lambda x: x > 0, []) is None
 
 
-'''def test_conditional_reduce():
+def test_conditional_reduce():
     assert functional.conditional_reduce(lambda x: x < 5, lambda x, y: x + y, [1, 3, 5, 10]) == 4
+    assert functional.conditional_reduce(lambda x: x > 0, lambda x, y: x * y, [1, 3, 5, 10]) == 150
+    assert functional.conditional_reduce(lambda x: x < 5, lambda x, y: x + y, []) is None
 
 
 def test_func_chain():
-    my_chain = functional.func_chain(lambda x: x + 2, lambda x: x/4, x//4)
-    assert my_chain(37) == (9.75, 9)
+    my_chain_one = functional.func_chain(lambda x: x + 2, lambda x: (x/4, x//4))
+    assert my_chain_one(37) == (9.75, 9)
 
+    my_chain_two = functional.func_chain(np.square, np.sqrt, lambda x: x**3)
+    assert my_chain_two([1, 2, 3, 4, 5]) == [1, 8, 27, 64, 125]
 
+'''
 def test_multiple_partial():
     ax1_mean, ax1_max, ax1_sum = functional.multiple_partial(np.mean, np.max, np.sum, axis=1)
     assert None'''
