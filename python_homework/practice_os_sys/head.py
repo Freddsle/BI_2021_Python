88267f202daf273344ca6ff7c0db9b328a223084
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-12-12 22:59:23 +0300

    added head script

--- python_homework/practice_os_sys/head.py
+++ python_homework/practice_os_sys/head.py
@@ -5,49 +5,46 @@ import argparse
 
 def main(files, n_lines=10, filenames=True):
     '''
-    Print the last "n_lines" lines of each FILE to standard output.
+    Print the first "n_lines" lines of each FILE to standard output.
     '''
     if filenames:
         number_files = len(files)
 
         for file in files:
-
             if number_files > 1:
                 sys.stdout.write(' '.join(['====>', file, '<====\n']))
 
             with open(file) as f:
-                file_strings = f.readlines()
-                file_len = len(file_strings)
-                read_start = file_len - n_lines
+                counter = 0
 
-                if read_start < 0:
-                    read_start = 0
-
-                for line in file_strings[read_start: ]:
-                    sys.stdout.write(line)
+                for line in f:
+                    if counter < n_lines:
+                        sys.stdout.write(line)
+                        counter += 1
+                    else:
+                        break
 
     else:
-        file_len = len(files)
-        read_start = file_len - n_lines
-        if read_start < 0:
-            read_start = 0
 
-        for line in files[ read_start : ]:
-            sys.stdout.write(line + '\n')
+        for i, line in enumerate(files):
+            if i < n_lines:
+                sys.stdout.write(line + '\n')
+            else:
+                break
 
 
 if __name__ == '__main__':
     """
     Show help message: -h, -- help.
     """
-    desc_one = 'Print the last 10 lines of each FILE to standard output.'
+    desc_one = 'Print the first 10 lines of each FILE to standard output.'
     desc_two = 'With more than one FILE, precede each with a header giving the file name.'
     desc_three = 'With no FILE read standard input.'
 
     parser = argparse.ArgumentParser(description=' '.join([desc_one, desc_two, desc_three]))
 
     parser.add_argument("-n", "--lines", default=10, type=int,
-                        help="output the last NUM lines, instead of the last 10")
+                        help="output the last NUM lines, instead of the first 10")
     parser.add_argument("files", nargs='*', default=sys.stdin, help="PATH to the FILEs.")
     args = parser.parse_args()
 
