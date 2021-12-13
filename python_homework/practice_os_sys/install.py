c3eb7969c9d690893e4d93713ca9fe650fac04c6
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-12-13 18:22:21 +0300

    added install script

---
+++ python_homework/practice_os_sys/install.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+
+def main():
+    '''
+    Copy scripts from "./" to PATH.
+    '''
+
+    scripts = ['./wc.py', './tail.py', './sort.py', './rm.py',
+               './mkdir.py', './ls.py', './ln.py','./head.py',
+               './grep.py', './cp.py', './cat.py']
+
+    standart_path = '/usr/local/bin'
+    
+    if os.path.isdir(standart_path):
+        for script in scripts:
+            shutil.copy(script, standart_path)
+    
+    else:
+        now_paths = os.environ['PATH'].split(':')
+        
+        for script in scripts:
+            shutil.copy(script, now_paths)
+
+
+if __name__ == '__main__':
+    """
+    Show help message: -h, -- help.
+    """
+    main()
