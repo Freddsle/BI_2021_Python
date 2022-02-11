27a6fac7658c53e36267e968a1a698a493d0a903
Yuliya Burankova (Freddsle) <burankova.y@gmail.com>
2022-02-11 12:46:18 +0100

    added all 4 task

--- python_homework/classes_practice/practice.py
+++ python_homework/classes_practice/practice.py
@@ -1,15 +1,20 @@
 import datetime
-from Bio.Seq import Seq, back_transcribe
+import os.path
+import itertools
+import csv
 
-# 1. Class that describes hamsters.
-# Python class for hamsters.
-# For creation "hamster" object it is nedeed to know its name and age (in months).
-# Optionally, one can specify the type of hamster (for example, Syrian or Chinese). Default "Not defined".
+import matplotlib.pyplot as plt
+import numpy as np
+
+from Bio.Seq import Seq, back_transcribe
+from Bio import SeqIO
 
 
 class Hamster:
     '''
-    Python class for hamsters.
+    Python Class that describes hamsters.
+    For creation "hamster" object it is nedeed to know its name and age (in months).
+    Optionally, one can specify the type of hamster (for example, Syrian or Chinese). Default "Not defined".
     '''
 
     def __init__(self, name, age, hamster_type='Not defined'):
@@ -64,28 +69,62 @@ class Hamster:
 class RNASequence(str):
     '''
     Class for operations with RNA sequences. Parent class - str.
-    '''    
+    '''
     acid_type = 'RNA'
-
+    
     def __init__(self, rna_sequence):
         self.rna_sequence = rna_sequence
-
-
+        
     def RNA_translation(self):
         '''
         Translation - returns a string corresponding to the protein from the RNA,
         according to the standard code, using Biopython.
         '''
-        self.protein = Seq.translate(self.rna_sequence)
+        self.protein = Bio.Seq.translate(self.rna_sequence)
         return self.protein
-
-
+        
     def RNA_to_DNA(self):
         '''
-        Reverse transcription method - returns a string corresponding to the DNA from RNA.
+        Reverse transcription method - returns a string corresponding to the DNA from RNA, using Biopython.
         '''
-        self.template_dna = back_transcribe(self.rna_sequence)
+        self.template_dna = Bio.Seq.back_transcribe(self.rna_sequence)
         return self.template_dna
+
+
+class PositiveSet(set):
+    '''
+    Sets with only positive numbers.
+    Two methods supports: add and update (but not |=).
+    '''
+    
+    def __init__(self, old_numbers):
+        '''
+        Initializes PositiveSet, adding only positive numbers using the add method.
+        '''
+        for number in old_numbers:
+            self.add(number)
+    
+    def add(self, element):
+        '''
+        Check if number for adding is positive. 
+        If so, it adds it to the PositiveSet using the regular set 'add' method.
+        '''
+        if element >= 0:
+            super().add(element)
+        
+    def update(self, *args):
+        '''
+        Update PositiveSet using positive values (PositiveSet 'add' method) from the given args.
+        '''
+        if len(args) == 1:
+            for element in args[0]:
+                self.add(element)
+        else:
+            for added_set in args[:-1]:
+                for element in added_set:
+                    self.add(element)
+    
+
 
 
 def main():
