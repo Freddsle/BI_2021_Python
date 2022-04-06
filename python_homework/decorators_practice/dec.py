b1e6f4173e809a374b9068cde6df91c2e3e3523a
Yuliya Burankova (Freddsle) <burankova.y@gmail.com>
2022-04-06 20:16:32 +0200

    all tasks done

--- python_homework/decorators_practice/dec.py
+++ python_homework/decorators_practice/dec.py
@@ -1,13 +1,39 @@
-from multiprocessing import reduction
 import time
 import random
+from unittest import result
 import requests
+import re
+import inspect
+
+
+def staticmethod_analog(func):
+    """
+    Decorator similar to staticmethod.
+    """
+    @classmethod
+    def inner_function(*args, **kwargs):
+        return func(*args[1:], **kwargs)
+    return inner_function
+
+
+class Apple:
+    """
+    Example class for staticmethod_analog decorator.
+    """
+
+    def __init__(self, ap_type, number):
+        self.ap_type = ap_type
+        self.number = number
+
+    @staticmethod_analog
+    def about_apple(first=1, second=2):
+        print('Apple is good for you.')
+        print(first, second)
 
 
 def measure_time(func):
     """
     Простой декоратор, подменивающий возвращаемое значение декорируемой функции на время её выполнения. 
-
     """
     def inner_function(*args, **kwargs):
         start = time.time()
@@ -41,10 +67,10 @@ def function_logging(func):
 
         if args and kwargs:
             print(f'Function {func.__name__} is calles with pisitional arguments {args} and keyword arguments: {", ".join(kw_print)}.')
-        
+
         elif  args:
             print(f'Function {func.__name__} is calles with pisitional arguments {args} and no keyword.')
-            
+
         elif  kwargs:
             print(f'Function {func.__name__} is calles with no pisitional arguments and keyword arguments: {", ".join(kw_print)}.')
 
@@ -116,42 +142,76 @@ def make_request(url):
     return requests.get(url)
 
 
-def staticmethod_analog(func):
+# Реализуйте декоратор аналогичный dataclass.
+# Весь функционал сделать как у стандартного dataclass за исключением того, 
+# что он не должен принимать дополнительные аргументы. 
+# При работе с декораторами классов очень полезными могут оказаться встроенные в питон функции setattr и getattr. 
+# Также вам нужно будет найти способ "распарсить" аннотации типов. 
+
+def dataclass_analog(cls):
+
+    class NEW_CLASS(cls):
+
+        def __init__(self, *args, **kwargs) -> None:
+            self.__dict__.update(kwargs)
+            i = 0
+
+            for key in cls.__annotations__:
+                if key not in self.__dict__:
+                    setattr(self, key, cls.__annotations__[key](args[i]))
+                    i += 1
+
+        def __repr__(self):
+            if '__repr__' not in cls.__dict__.keys():
+                attrs = [f"{key}={value}" for key, value in self.__dict__.items()]
+                return f'{type(self).__name__}({", ".join(attrs)})'
+            else:
+                return super().__repr__()
+
+        def __eq__(self, other):
+            if '__eq__' not in cls.__dict__.keys():
+                if other.__class__ is self.__class__:
+                    return list(self.__dict__.values()) == list(other.__dict__.values())
+                return NotImplemented
+            else:
+                return super().__repr__()
+
+        def __match_args__(self):
+            if '__match_args__' not in cls.__dict__.keys():
+                return  tuple(self.__dict__.keys())
+                
+            else:
+                return super().__match_args__()
+
+    NEW_CLASS.__name__ = cls.__name__
+
+    return NEW_CLASS
+
+    
+@dataclass_analog   
+class InventoryItem:
     """
-    Decorator similar to staticmethod.
+    Example class for dataclass_analog decorator.
+    Class for keeping track of an item in inventory.
     """
-    def inner_function(*args, **kwargs):
+    name: str
+    unit_price: float
+    quantity_on_hand: int = 0
 
-        if args:
-            if '__main__' in str(args[0]):
-                return func(*args[1:], **kwargs)
-        else:
-            return func(*args, **kwargs)
+    def total_cost(self) -> float:
+        return self.unit_price * self.quantity_on_hand
 
-    return inner_function
-
-
-class Apple:
-    """
-    Example class for staticmethod_analog decorator.
-    """
-
-    def __init__(self, ap_type, number):
-        self.ap_type = ap_type
-        self.number = number
-
-    @staticmethod_analog
-    def about_apple(first=1, second=2):
-        print('Apple is good for you.')
-        print(first, second)
+    # Test for "repr"
+    # def __repr__(self) -> str:
+    #    return 'Test for "repr" - super.'
 
 
 if __name__ == '__main__':
     # Example run for measure_time decorator
-    #print(some_function(1, 2, 3, 4, e=5, f=6, g='9999'))
+    # print(some_function(1, 2, 3, 4, e=5, f=6, g='9999'))
     
     # Example run for function_logging decorator
-    #print(func1(), end='\n\n')
+    # print(func1(), end='\n\n')
     #print(func2(1 ,2, 3), end='\n\n')
     #print(func3(1 ,2, c=3, d=2), end='\n\n')
     #print(func4(a=None, b=float("-inf")), end='\n\n')
@@ -163,3 +223,17 @@ if __name__ == '__main__':
     # Example run for staticmethod_analog decorator
     #Apple('gold', 5).about_apple()
     #Apple.about_apple()
+
+    # Example run for dataclass_analog decorator
+    invemtory = InventoryItem('pen', 23.0, 3)
+    print(f'invemtory.name: \t {invemtory.name}')
+    print(f'invemtory.unit_price: \t {invemtory.unit_price}')
+    print(f'invemtory.quantity_on_hand: \t {invemtory.quantity_on_hand}')
+    print(f'invemtory.total_cost: \t {invemtory.total_cost()}')
+    print(f'invemtory: \t {invemtory}')
+
+    invemtory2 = InventoryItem('pen', 15.0, 2)
+    print(f'invemtory: \t {invemtory.__eq__(invemtory2)}')
+
+    invemtory3 = InventoryItem('pen', 15.0, 2)
+    print(invemtory3.__match_args__())
